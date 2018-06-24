776c63d119638166fd7ec3f59e30f3b51df3a107
alisabrigok <alisabrigok@gmail.com>
2018-06-24 12:26:52 +0300

    Comment Typo

--- src/AppBundle/Models/Currency.php
+++ src/AppBundle/Models/Currency.php
@@ -16,8 +16,9 @@ class Currency
             // convert the object to array
             $flattenCurrencies = get_object_vars($currencyObjElement);
             foreach ($flattenCurrencies as $flattenElement) {
-                // skip every even property
+                // skip every even property, because values are in an array and 1st element (index 0) is always currency name
                 if ($i % 2 !== 0) {
+                    // convert the value to float
                     $floatElement = floatval($flattenElement);
                     array_push($currency, $floatElement);
                 }
